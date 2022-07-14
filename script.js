5036bc472965d7722b8ab809056921d8f6657d1f
Dev <devanandv12@gmail.com>
2022-07-14 18:14:40 +0530

    COmpleted Html and Css with popup

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".popup").style.display="flex";
+})
+
+document.getElementById("close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display="none";
+})
