722072e380bd1805a381783b5adeca65b883468d
Dev <devanandv12@gmail.com>
2022-07-15 17:46:08 +0530

    Skeleton JS complete

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*POPUP SCRIPT*/
 document.getElementById("button").addEventListener("click", function(){
     document.querySelector(".popup").style.display="flex";
 })
@@ -5,3 +6,26 @@ document.getElementById("button").addEventListener("click", function(){
 document.getElementById("close").addEventListener("click", function(){
     document.querySelector(".popup").style.display="none";
 })
+
+
+
+
+/* LIBRARY SCRIPT*/
+let myLibrary = [];
+
+function Book(title, author, pages, read, ){
+    this.title = title;
+    this.author = author;
+    this.pages=pages;
+    this.read=read; 
+    this.info = function(){
+        return title + author + pages+ read;
+    }
+}
+
+//Function for adding new book to the array
+
+function addBookToLibrary(title, author, pages, read){
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
